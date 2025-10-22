36db49c7373fc398fec638cc0a5b3649cced39bf
Misagh <misaghsoltani@gmail.com>
2025-10-22 02:00:34 -0400

    Fix an issue with Gymnasium registration, bump version to 0.1.1 and update Python version constraints to <3.15 in package dependencies.

--- numberlink/__init__.py
+++ numberlink/__init__.py
@@ -19,8 +19,10 @@ from .registration import register_numberlink_v0
 from .vector_env import NumberLinkRGBVectorEnv
 from .viewer import NumberLinkViewer
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Misagh Soltani"
+
+register_numberlink_v0()
 
 __all__: list[str] = [
     "__version__",
